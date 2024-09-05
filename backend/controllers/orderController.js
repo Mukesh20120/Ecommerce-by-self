85d2a518d0daee2baefe4c6e8a99fa023f057300
Mukesh <mukesh@algogenix.com>
2024-09-05 17:46:14 +0530

    Backend -fix the error not getting the user id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,14 +1,19 @@
 const asyncWrapper = require("../middleware/AsyncWrapper");
-const customError = require('../errors');
-const {Product,Order} = require('../models');
-const calcPrices = require('../utils/calPrices')
+const customError = require("../errors");
+const { Product, Order } = require("../models");
+const calcPrices = require("../utils/calPrices");
 
 const addOrderItems = asyncWrapper(async (req, res) => {
-  const {orderItems,shippingAddress,paymentMethod} = req.body;
-  if(!orderItems || orderItems.length<=0){
-    throw new customError.NotFoundError('order Items not found send order item');
+  const { orderItems, shippingAddress, paymentMethod } = req.body;
+  console.log(req.body);
+  if (!orderItems || orderItems.length <= 0) {
+    throw new customError.NotFoundError(
+      "order Items not found send order item"
+    );
   }
-  const itemsFromDB = await Product.find({_id: {$in: orderItems.map(x=>x._id)}});
+  const itemsFromDB = await Product.find({
+    _id: { $in: orderItems.map((x) => x._id) },
+  });
   const dbOrderItems = orderItems.map((itemFromClient) => {
     const matchingItemFromDB = itemsFromDB.find(
       (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
@@ -27,7 +32,7 @@ const addOrderItems = asyncWrapper(async (req, res) => {
 
   const order = new Order({
     orderItems: dbOrderItems,
-    user: req.user.userId,
+    user: req.user._id,
     shippingAddress,
     paymentMethod,
     itemsPrice,
@@ -41,15 +46,18 @@ const addOrderItems = asyncWrapper(async (req, res) => {
   res.status(201).json(createdOrder);
 });
 const getMyOrders = asyncWrapper(async (req, res) => {
-  const order = await Order.find({_id: req.user.userId})
-  res.json({ success: true,order });
+  const order = await Order.find({ _id: req.user.userId });
+  res.json({ success: true, order });
 });
 const getOrderById = asyncWrapper(async (req, res) => {
-  const order = await Order.findById({_id: req.params.id}).populate('user','name email');
-  if(!order){
-    throw new customError.NotFoundError('order not found provide valid user');
+  const order = await Order.findById({ _id: req.params.id }).populate(
+    "user",
+    "name email"
+  );
+  if (!order) {
+    throw new customError.NotFoundError("order not found provide valid user");
   }
-  res.json({ success: true,order });
+  res.json({ success: true, order });
 });
 const updateOrderToPaid = asyncWrapper(async (req, res) => {
   res.json({ msg: "updateOrderToPaid order successfully" });
@@ -58,8 +66,8 @@ const updateOrderToDelivered = asyncWrapper(async (req, res) => {
   res.json({ msg: "updateOrderToDelivered order successfully" });
 });
 const getOrders = asyncWrapper(async (req, res) => {
-  const orders = await Order.find({}).populate('user', 'id name')
-  res.json({ success: true,orders });
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json({ success: true, orders });
 });
 
 module.exports = {
